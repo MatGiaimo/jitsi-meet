a5ba902548d53ecc4603a4eb8c2cbb2f78c59503
Mat Giaimo <mgiaimo@gmail.com>
2020-11-09 21:56:11 -0500

    Revert "Merge pull request #2 from setpill/tile-view-maximize-space-usage"
    This reverts commit 334bb67bf05359c90b19eb57b87e0eb132cff1a9.

--- react/features/video-layout/functions.js
+++ react/features/video-layout/functions.js
@@ -37,49 +37,30 @@ export function getMaxColumnCount() {
 }
 
 /**
- * Returns the cell count dimensions for tile view. Tile view tries to fill
- * the available space optimally.
+ * Returns the cell count dimensions for tile view. Tile view tries to uphold
+ * equal count of tiles for height and width, until maxColumn is reached in
+ * which rows will be added but no more columns.
  *
  * @param {Object} state - The redux store state.
+ * @param {number} maxColumns - The maximum number of columns that can be
+ * displayed.
  * @returns {Object} An object is return with the desired number of columns,
  * rows, and visible rows (the rest should overflow) for the tile view layout.
  */
-export function getTileViewGridDimensions(state: Object) {
+export function getTileViewGridDimensions(state: Object, maxColumns: number = getMaxColumnCount()) {
     // When in tile view mode, we must discount ourselves (the local participant) because our
     // tile is not visible.
     const { iAmRecorder } = state['features/base/config'];
     const numberOfParticipants = state['features/base/participants'].length - (iAmRecorder ? 1 : 0);
-    const { clientHeight, clientWidth } = state['features/base/responsive-ui'];
-    const aspectRatio = state['features/filmstrip'].TILE_ASPECT_RATIO;
 
-    let bestColumns = 0;
-    let bestRows = 0;
-    let bestTileWidth = 0;
-    let bestFound = false;
-
-    while (!bestFound) {
-        const columns = bestColumns + 1;
-        const straightTileWidth = Math.floor(clientWidth / columns);
-
-        const rows = Math.ceil(numberOfParticipants / columns);
-        const straightTileHeight = Math.floor(clientHeight / rows);
-
-        const constrainedTileWidth = Math.ceil(straightTileHeight * aspectRatio);
-
-        const tileWidth = Math.min(straightTileWidth, constrainedTileWidth);
-
-        if (tileWidth > bestTileWidth) {
-            bestColumns = columns;
-            bestRows = rows;
-            bestTileWidth = tileWidth;
-        } else {
-            bestFound = true;
-        }
-    }
+    const columnsToMaintainASquare = Math.ceil(Math.sqrt(numberOfParticipants));
+    const columns = Math.min(columnsToMaintainASquare, maxColumns);
+    const rows = Math.ceil(numberOfParticipants / columns);
+    const visibleRows = Math.min(maxColumns, rows);
 
     return {
-        columns: bestColumns,
-        visibleRows: bestRows
+        columns,
+        visibleRows
     };
 }
 
